9722f8b40cc4d10526347cc739d97ab842e8caf5
Gerrit Oomens <g.oomens@uva.nl>
2023-04-29 13:15:36 +0200

    Add db password and fix Dockerfile

--- apltesting/apltesting/settings.py
+++ apltesting/apltesting/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.environ.get('SQL_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
         'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASS', 'pass'),
         'HOST': os.environ.get('SQL_HOST', 'localhost'),
         'PORT': os.environ.get('SQL_PORT', '5432'),
     }
--- apltesting/docker-entrypoint.prod.sh
+++ apltesting/docker-entrypoint.prod.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python3 manage.py migrate --noinput
-python3 manage.py loaddata tests --noinput
-python3 manage.py loaddata questions --noinput
+python3 manage.py loaddata tests
+python3 manage.py loaddata questions
 python3 manage.py collectstatic --no-input --clear
 
-exec "$@"
+gunicorn apltesting.wsgi -b 0.0.0.0:8007
